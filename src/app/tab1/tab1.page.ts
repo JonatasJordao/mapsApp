52b4eaa3d928bde703c5d2d0f01b837485d093d0
JonatasJordao <jonatashjoliveira@gmail.com>
2022-08-25 14:37:53 -0300

    2 files modified

--- src/app/tab1/tab1.page.ts
+++ src/app/tab1/tab1.page.ts
@@ -1,12 +1,14 @@
-import { Component } from '@angular/core';
+import { Component, ElementRef, ViewChild } from '@angular/core';
 
 @Component({
   selector: 'app-tab1',
   templateUrl: 'tab1.page.html',
-  styleUrls: ['tab1.page.scss']
+  styleUrls: ['tab1.page.scss'],
 })
 export class Tab1Page {
+  @ViewChild('map') mapRef: ElementRef;
+
+  map: google.maps.Map;
 
   constructor() {}
-
 }
